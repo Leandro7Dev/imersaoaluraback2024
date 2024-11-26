13337a969b755aba010ff5998cc45dc633f60617
Leandro7Dev <leandroarti@gmail.com>
2024-11-25 22:47:22 -0300

    first commit

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -4,3 +4,11 @@ export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
 }
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost)
+        res.status(200).json(postCriado);
+    }
+}
