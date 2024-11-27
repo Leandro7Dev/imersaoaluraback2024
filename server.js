be8c96e13464f696facc694349e70c477f3013ce
Leandro7Dev <leandroarti@gmail.com>
2024-11-27 16:46:28 -0300

    commit 02

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./src/routes/postsRouts.js";
 
 const app = express();
+app.use(express.static("uploads"))
 routes(app)
 
 app.listen(3000, () => {
